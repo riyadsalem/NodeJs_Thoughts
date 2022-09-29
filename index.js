ad0d20d619862c7444acc5bd682ecb810c4d7e11
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-09-29 07:58:44 +0300

    GET All Products, in POSTMAN

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+const products = require("./products");
+app.listen(3000, () => console.log("SERVER is running at port 3000"));
 
 app.get("/", (req, res) => {
   res.send("HOME PAGE");
 });
 
-app.listen(3000, () => console.log("SERVER is running at port 3000"));
+/*
+ * Show List Of Products
+ * Show Specific products
+ * Insert A Product Data
+ * Update Specific Product Data ( Using PUT Method)
+ * Update Specific Proudct Data (Using PATCH Method)
+ * Delete A Specific Product Data
+ * Delete All Preoducts Data
+ */
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
---
+++ products.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    id: 1,
+    name: "Orange",
+    price: 20,
+  },
+  {
+    id: 2,
+    name: "Apple",
+    price: 30,
+  },
+];
+
+module.exports = products;
