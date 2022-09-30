382b86218e4f645ccb3543a35612f6e14f7b8dcb
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-09-30 08:15:46 +0300

    Update a Product Information with PUT Method

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.get("/", (req, res) => {
 });
 
 /*
- * Update Specific Product Data ( Using PUT Method)
  * Update Specific Proudct Data (Using PATCH Method)
  * Delete A Specific Product Data
  * Delete All Preoducts Data
@@ -32,15 +31,19 @@ app.get("/api/products/:id", (req, res) => {
     : res.json(product);
 });
 
-// Insert A Product Data
-app.use(express.json());
-app.post("/api/insertProducts", (req, res) => {
+const validation = (body) => {
   const schema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     price: Joi.number().required(),
   });
 
-  const { error } = schema.validate(req.body);
+  return schema.validate(body);
+};
+
+// Insert A Product Data
+app.use(express.json());
+app.post("/api/insertProducts", (req, res) => {
+  const { error } = validation(req.body);
 
   if (error) {
     return res.status(400).json({
@@ -59,4 +62,39 @@ app.post("/api/insertProducts", (req, res) => {
   if (!products) res.status(404).json({ error: "No Products Found " });
 
   return res.json(products);
+});
+
+// Update Specific Product Data ( Using PUT Method)
+app.put("/api/updateProductPUT/:id", (req, res) => {
+  const { value, error } = validation(req.body);
+
+  if (error) {
+    return res.status(400).json({
+      message: error.details[0].message,
+    });
+  }
+
+  /*
+  const { id } = req.params;
+  const product = products.find((prod) => prod.id === Number(id));
+  product.name = req.body.name;
+  product.price = req.body.price;
+  return res.json(product);
+  */
+
+  const index = products.findIndex((prod) => prod.id === Number(req.params.id));
+
+  if (index === -1) {
+    return res.status(404).json({
+      message: "Product is not found with this ID",
+    });
+  }
+
+  const { name, price } = req.body;
+  products[index].name = name;
+  products[index].price = price;
+
+  return res.json({
+    products: products[index],
+  });
 });
