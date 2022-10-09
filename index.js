c035f8fdcb155b552294d962d18c6276b3234ec2
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-10-09 08:39:03 +0300

    Schema and Models

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost:27017/test")
-  .then(() => console.log("Database is connected"))
+  .then(() => console.log("DATABASE IS CONNECTED"))
   .catch((error) => console.log(error));
+
+// Schema
+// Document , collection, database
+/*
+const user = [
+  { name: "user1", age: 24 },
+  { name: "user2", age: 30 },
+];
+*/
+
+const userShcema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  isMarried: Boolean,
+  salary: Number,
+  gender: String,
+});
+
+const User = mongoose.model("User", userShcema);
