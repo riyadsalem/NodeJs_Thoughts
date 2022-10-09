7c1b99a6ea5873db4e03b150dfc856107fc0d8db
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-10-09 15:00:29 +0300

    Update a DOCUMENT ( Way 1 )

--- index.js
+++ index.js
@@ -69,23 +69,34 @@ async function fetchInformation() {
 fetchInformation();
 */
 
+/*
 async function db() {
   // const users = await User.find().or([{ isMarried: true }, { age: 30 }]);
   // const users = await User.find().and([{ isMarried: false }, { age: 30 }]);
   // const users = await User.find({ isMarried: false, age: 30 }); // and([{},{}])
 
-  /*
+  
   // Problem Solve
   // Find Those users whose age is greater thaen 40 or they are unmarried 
   // find only name 
   // shorted them by name
-  */
+  
 
   const users = await User.find()
     .or([{ age: { $gt: 40 } }, { isMarried: false }])
     .select("name")
-    .sort("name"); // A B C D E .....
+    .sort("name"); // A B C D E ..... 
 
   console.log(users);
 }
 db();
+*/
+
+async function updateInformation() {
+  const user = await User.findById("634279640efbc70b5203cbec");
+  user.isMarried = true;
+  await user.save();
+  console.log(user);
+}
+
+updateInformation();
